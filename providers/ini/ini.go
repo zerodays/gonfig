ef963eeaec239279fe57fa8f3a130e5cf27bedb3
Vid Drobnic <vid.drobnic@gmail.com>
2020-11-07 21:48:54 +0100

    improved ini provider

--- providers/ini/ini.go
+++ providers/ini/ini.go
@@ -2,9 +2,23 @@ package ini
 
 import (
 	"gopkg.in/ini.v1"
+	"strings"
+)
+
+type Mode int
+
+const (
+	ModeNormal Mode = iota
+	ModeUppercase
+	ModeLowercase
 )
 
 type Provider struct {
+	// SectionMode and KeyMode determine weather the section and key parameters
+	// should be automatically converted to lower or upper case.
+	// Normally SectionMode should be ModeLowercase and KeyMode should be ModeUppercase.
+	SectionMode, KeyMode Mode
+
 	file *ini.File
 }
 
@@ -20,22 +34,43 @@ func New(source interface{}) (*Provider, error) {
 	}, nil
 }
 
+// key returns Key for specified section and name.
+func (p *Provider) key(section, name string) *ini.Key {
+	// Convert section to lower or upper case based on mode.
+	switch p.SectionMode {
+	case ModeUppercase:
+		section = strings.ToUpper(section)
+	case ModeLowercase:
+		section = strings.ToLower(section)
+	}
+
+	// Convert name to lower or upper case based on mode.
+	switch p.KeyMode {
+	case ModeUppercase:
+		name = strings.ToUpper(name)
+	case ModeLowercase:
+		name = strings.ToLower(name)
+	}
+
+	return p.file.Section(section).Key(name)
+}
+
 // GetString gets string from ini file for specified parameters.
 func (p *Provider) GetString(_, section, name string) (string, error) {
-	return p.file.Section(section).Key(name).String(), nil
+	return p.key(section, name).String(), nil
 }
 
 // GetInt gets int from ini file for specified parameters.
 func (p *Provider) GetInt(_, section, name string) (int, error) {
-	return p.file.Section(section).Key(name).Int()
+	return p.key(section, name).Int()
 }
 
 // GetBool gets bool from ini file for specified parameters.
 func (p *Provider) GetBool(_, section, name string) (bool, error) {
-	return p.file.Section(section).Key(name).Bool()
+	return p.key(section, name).Bool()
 }
 
 // GetFloat gets float from ini file for specified parameters.
 func (p *Provider) GetFloat(_, section, name string) (float64, error) {
-	return p.file.Section(section).Key(name).Float64()
+	return p.key(section, name).Float64()
 }
